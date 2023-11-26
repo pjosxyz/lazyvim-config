87bbe04dbc6460bdaf03ae093929d8a8a87248fc
pjosxyz <pjosxyz@gmail.com>
2023-11-26 18:59:03 +0000

    Added treesitter stuff

---
+++ plugins/treesitter.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "go",
+        "http",
+        "java",
+        "scss",
+        "sql",
+        "svelte",
+        "html",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
